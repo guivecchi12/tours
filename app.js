5707053ff2988bdc435063fe446cdce591b2696e
Gui Vecchi <guivecchi12@gmail.com>
2024-03-21 15:37:55 -0600

    implement cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -25,6 +26,13 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 // GLOBAL Middlewares
+// Implement CORS
+app.use(
+  cors({
+    origin: 'https://www.gvecchi.com'
+  })
+)
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')))
 
